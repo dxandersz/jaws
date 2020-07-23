211c131be33abf69ac261ece16829bf7c08d5563
David Anderson <d.c.anderson@live.com>
2020-07-23 01:38:38 -0400

    Move function is completely working and refactored for easier implementation for both the shark and the player.

--- jawsTheGame/test.js
+++ jawsTheGame/test.js
@@ -1,21 +1,29 @@
 let shark_position = 1;
 let player_position = 1; 
+let available_moves = []
 
 
 // The base unit for the board size. Square this to make the board tiles. The key value for a lot of tile trait calculations.
-board_base = 5
-board_size = board_base * board_base
+let board_base = 5;
+let board_size = board_base * board_base;
 // Important! At least until I fold in something more sophisticated. Since it's the only designated land space, the code will be referencing it frequently
 board_center = Math.ceil(board_size/2);
 
-//The number of cells on the board you'll need to move in either direction to go NESW
-let directions = [-1, board_base, 1, -(board_base)]
+//Assigning easy to call variables for the directions. Should stay constant for any board with an odd board_base.
+const north = -1;
+const east = board_base;
+const south = 1;
+const west = -board_base;
 
-
+console.log(north)
+console.log(south)
+console.log(east)
+console.log(west)
 //Spielberg directs the board, evaluating decisions made by the player or shark.
 
 const spielberg = {
     //determines the shark's location on the board.
+
     sharkStart: function() {
         //Since the center square will always be land, we exclude it. Will need to refactor if I want to mix in more complex maps.
         sharkIndex = Math.ceil(Math.random() * (board_size - 1));
@@ -30,8 +38,41 @@ const spielberg = {
         }
 
     },
-    //So we can run through  
-    index: 0,
+    // Takes a given tile and figures out its relationship to various objects. Takes the value of a tile, as numbered in index.html
+    canMove: function(tile) {
+        // holds the  values for NESW after evaluation. This hopefully keeps the moves options abstract without being unwieldy.
+        directions = []
+        // Evaluates whether North is true.
+        if ((tile + north) % board_base === 0 || tile + north === board_center) {
+            console.log("You can't move North.");
+        }
+        else {
+            console.log("You can move North");
+            directions.push(north);
+        }
+        if (tile > (board_size - board_base)  || tile + east === board_center) {
+            console.log("You can't move East.")
+        }
+        else {
+            console.log("You can move East.");
+            directions.push(east);
+        }
+        if (tile % board_base === 0 || tile + south === board_center) {
+            console.log("You can't move South.");
+        }
+        else {
+            console.log("You can move South.");
+            directions.push(south);
+        }
+        if (tile <= board_base  || tile + west === board_center) {
+            console.log("You can't move West.")
+        }
+        else {
+            console.log("You can move West.")
+            directions.push(west)
+        }
+        console.log(directions)
+    },
     //Evaluates whether a tile can be moved on but also whether or not it triggers a special event.
     tileType: [],
     // Stores whether or not you can move NESW from this tile.
@@ -41,3 +82,5 @@ const spielberg = {
 
 console.log(shark_position);
 spielberg.sharkStart();
+spielberg.canMove(shark_position);
+console.log(directions);
