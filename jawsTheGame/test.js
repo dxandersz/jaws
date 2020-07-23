72a3df8aa03904bae57c6327992f4c63c4befd1c
David Anderson <d.c.anderson@live.com>
2020-07-23 00:38:49 -0400

    Refactored some actions and logic into a 'director' object that will oversee the board state.

--- jawsTheGame/test.js
+++ jawsTheGame/test.js
@@ -4,27 +4,40 @@ let player_position = 1;
 
 // The base unit for the board size. Square this to make the board tiles. The key value for a lot of tile trait calculations.
 board_base = 5
+board_size = board_base * board_base
+// Important! At least until I fold in something more sophisticated. Since it's the only designated land space, the code will be referencing it frequently
+board_center = Math.ceil(board_size/2);
 
 //The number of cells on the board you'll need to move in either direction to go NESW
 let directions = [-1, board_base, 1, -(board_base)]
 
 
-//Used to evaluate a tile for traits. I deally I don't even need to build an array. I just keep reassigning variables and checking them against conditions for those variables.
+//Spielberg directs the board, evaluating decisions made by the player or shark.
 
-function pullTile(tile) {
+const spielberg = {
+    //determines the shark's location on the board.
+    sharkStart: function() {
+        //Since the center square will always be land, we exclude it. Will need to refactor if I want to mix in more complex maps.
+        sharkIndex = Math.ceil(Math.random() * (board_size - 1));
+        console.log(sharkIndex);
+        if (sharkIndex < board_center) {
+            shark_position = sharkIndex;
+            console.log("Under 13: " + shark_position)
+        }
+        else {
+            shark_position = sharkIndex + 1;
+            console.log("Over 13: " + shark_position)
+        }
 
-}
-
-const Tile = {
+    },
     //So we can run through  
     index: 0,
     //Evaluates whether a tile can be moved on but also whether or not it triggers a special event.
     tileType: [],
     // Stores whether or not you can move NESW from this tile.
-    //Is there a barrel on this space?
-    tagged: false,
+    //Tracks the barrels
+    tagged: [],
 };
 
-
-let num = 2068
-console.log(num.toString() + ' blah blah blah');
+console.log(shark_position);
+spielberg.sharkStart();
